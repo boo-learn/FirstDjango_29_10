652462037bda18b670bb7479083454043e3a719c
boo <eyurchenko@specialist.ru>
2022-11-05 18:13:18 +0700

    Приводим проект в порядок

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -3,7 +3,6 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.main),
-    path('about', views.about),
-    path('item/<int:id>', views.get_item),
     path('items', views.all_items),
+    path('item/<int:id>', views.get_item),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,33 +3,9 @@ from django.http import HttpResponseNotFound
 from MainApp.models import Item
 from django.core.exceptions import ObjectDoesNotExist
 
-author = {
-    "name": "Евгений",
-    "surname": "Юрченко",
-    "midlename": "Витальевич",
-}
-
-items = [
-    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-    {"id": 3, "name": "Coca-cola 1 литр", "quantity": 12},
-    {"id": 4, "name": "Картофель фри", "quantity": 0},
-    {"id": 5, "name": "Кепка", "quantity": 124},
-    {"id": 6, "name": "Попкорн", "quantity": 4},
-]
-
 
 def main(request):
     return render(request, 'index.html')
-
-
-def about(request):
-    text = f"""
-    Имя: <b>{author['name']}</b><br>
-    Отчество: <b>{author['midlename']}</b><br>
-    Фамилия: <b>{author['surname']}</b><br>
-    """
-    return HttpResponse(text)
 
 
 # /item/4
